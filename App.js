0f20d9d7eb96820e5c971e95c594072491c16c50
G1betm <korvinamber1@mail.ru>
2020-07-04 15:44:13 +0500

    release v1

--- App.js
+++ App.js
@@ -1,21 +1,158 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SectionList } from 'react-native';
+import styled from 'styled-components/native' ;
+import { Ionicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons'; 
+
+import { Appointment, SectionTitle } from "./components";
+
+const DATA = [
+    {
+      title: "30 июня",
+      data: [
+        {
+            time: '15:30',
+            diagnosis: 'тут какой-то текст',
+            active: true,
+            user: {
+                fullname: 'Алишер Ахмедов',
+                avatar: 'https://sun9-21.userapi.com/c857532/v857532576/125124/weqq4JUW9P0.jpg?ava=1',
+            }
+        },
+
+        {
+            time: '16:40',
+            diagnosis: 'тут какой-то текст 2',
+            user: {
+                fullname: 'Мухаммадяхё Аскаров ',
+                avatar: 'https://sun9-7.userapi.com/c853528/v853528508/122d10/q7nQ4ruIRDg.jpg?ava=1',
+            }
+        },
+        {
+          time: '15:30',
+          diagnosis: 'тут какой-то текст',
+          
+          user: {
+              fullname: 'Алишер Ахмедов',
+              avatar: 'https://sun9-21.userapi.com/c857532/v857532576/125124/weqq4JUW9P0.jpg?ava=1',
+          }
+      },
+
+      {
+          time: '16:40',
+          diagnosis: 'тут какой-то текст 2',
+          user: {
+              fullname: 'Мухаммадяхё Аскаров ',
+              avatar: 'https://sun9-7.userapi.com/c853528/v853528508/122d10/q7nQ4ruIRDg.jpg?ava=1',
+          }
+      },
+
+    ]
+    },
+    {
+      title: "1 июля",
+      data: [
+        {
+            time: '15:30',
+            diagnosis: 'тут какой-то текст 3',
+            user: {
+                fullname: 'Артур Файзуллин',
+                avatar: 'https://sun9-15.userapi.com/c840532/v840532687/752e4/ZjsSwtv6fFs.jpg?ava=1',
+            }
+        },
+        {
+          time: '15:30',
+          diagnosis: 'тут какой-то текст',
+          
+          user: {
+              fullname: 'Алишер Ахмедов',
+              avatar: 'https://sun9-21.userapi.com/c857532/v857532576/125124/weqq4JUW9P0.jpg?ava=1',
+          }
+      },
+
+      {
+          time: '16:40',
+          diagnosis: 'тут какой-то текст 2',
+          user: {
+              fullname: 'Мухаммадяхё Аскаров ',
+              avatar: 'https://sun9-7.userapi.com/c853528/v853528508/122d10/q7nQ4ruIRDg.jpg?ava=1',
+          }
+      },
+      {
+        time: '15:30',
+        diagnosis: 'тут какой-то текст',
+        
+        user: {
+            fullname: 'Алишер Ахмедов',
+            avatar: 'https://sun9-21.userapi.com/c857532/v857532576/125124/weqq4JUW9P0.jpg?ava=1',
+        }
+    },
+
+    {
+        time: '16:40',
+        diagnosis: 'тут какой-то текст 2',
+        user: {
+            fullname: 'Мухаммадяхё Аскаров ',
+            avatar: 'https://sun9-7.userapi.com/c853528/v853528508/122d10/q7nQ4ruIRDg.jpg?ava=1',
+        }
+    },
+    ]
+    }, 
+  ];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+        
+        <SectionList
+            sections={DATA}
+            keyExtractor={(item, index) => index}
+            renderItem={({ item }) => <Appointment { ...item } /> } 
+            renderSectionHeader={({ section: { title } }) => (
+        <SectionTitle>{title}</SectionTitle>
+      )}
+    />
+    <PlusButton style={{
+        shadowColor: "#2A86FF",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 4.65,
+        
+        elevation: 6,
+    }}>
+        <Entypo name="plus" size={32} color="white" />
+    </PlusButton>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+    flex: 1;
+    margin-top: 50px;
+    
+`;
+
+const PlusButton = styled.TouchableOpacity`
+    
+    position: absolute;
+    background: #2a86ff;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50px;
+    width: 64px;
+    height: 64px;
+    right: 25px;
+    bottom: 25px;
+    
+    
+`;
+
+
+
+
+
+
---
+++ components/Appointment.jsx
@@ -0,0 +1,75 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, SectionList } from 'react-native';
+import styled from 'styled-components/native' ;
+
+
+
+export default function Group({ user, diagnosis, active, time }) {
+    return (
+        
+        
+            
+    <GroupItem>
+        <Avatar
+         source={{
+         uri: user.avatar
+        }}
+        />
+        <View style={{flex: 1}}>
+            <FullName>{ user.fullname }</FullName>
+            <GrayText>{ diagnosis }</GrayText>
+        </View>
+        <GroupDate active={active}>{ time }</GroupDate>
+    </GroupItem>
+            
+    
+    );
+    
+};
+
+Group.defaultProps = {
+    title: 'Untitled',
+    items: []
+};
+
+
+
+
+const GroupItem = styled.TouchableOpacity`
+    padding: 20px;
+    flex-direction: row;
+    align-items: center;
+    border-bottom-width: 1px;
+    border-bottom-color: #F3F3F3;
+    
+`;
+const Avatar = styled.Image`
+    border-radius: 50px;
+    width: 50px;
+    height: 50px;
+    margin-right: 15px;
+`;
+const FullName = styled.Text`
+    font-weight: 600;
+    font-size: 16px;
+`;
+const GrayText = styled.Text`
+    font-size: 16px;
+    color: #8B979F;
+
+`;
+const GroupDate = styled.Text`
+    background: ${props => props.active ? '#2A86FF;' : '#E9F5FF'};
+    color: ${props => props.active ? '#fff;' : '#4294ff'};
+    border-radius: 18px;
+    padding: 7px 17px;
+    font-size: 16px;
+    align-items: center;
+    justify-content: center;
+    margin-left: 20px;
+    font-weight: bold;
+
+
+`;
+
